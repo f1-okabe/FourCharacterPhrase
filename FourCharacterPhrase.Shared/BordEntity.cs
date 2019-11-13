2f19d59ab060005f32e925b18ee0f75100f7be3b
岡部智幸 <okabe@f1-net.co.jp>
2019-11-13 13:00:21 +0900

    9

--- FourCharacterPhrase.Shared/BordEntity.cs
+++ FourCharacterPhrase.Shared/BordEntity.cs
@@ -16,9 +16,16 @@ namespace FourCharacterPhrase.Shared
 
         public List<WordEntity> Words { get; set; } = new List<WordEntity>();
 
+        public List<string> AllWords { get; set; } = new List<string>();
+
         public AnswerNumberEntity AnswerNumber { get; set; } = new AnswerNumberEntity();
 
         private DateTime startTime;
+
+        public BordEntity()
+        {
+            SetAllWords();
+        }
 
         public void SetData()
         {
@@ -31,17 +38,18 @@ namespace FourCharacterPhrase.Shared
 
         private void SetWords()
         {
+            var usedNumbers = new List<int>();
+
             Words = new List<WordEntity>();
 
-            Words.Add(new WordEntity() { Value = "悪戦苦闘" });
-            Words.Add(new WordEntity() { Value = "安楽浄土" });
-            Words.Add(new WordEntity() { Value = "暗中模索" });
-            Words.Add(new WordEntity() { Value = "阿鼻叫喚" });
-            Words.Add(new WordEntity() { Value = "異口同音" });
-            Words.Add(new WordEntity() { Value = "一網打尽" });
-            Words.Add(new WordEntity() { Value = "因果応報" });
-            Words.Add(new WordEntity() { Value = "以心伝心" });
-            Words.Add(new WordEntity() { Value = "意気消沈" });
+            while (Words.Count <= 9)
+            {
+                var randomNumber = RandomNumber(AllWords.Count);
+                if (Words.Any(m => m.Value == AllWords[randomNumber]) == false)
+                {
+                    Words.Add(new WordEntity() { Value = AllWords[randomNumber] });
+                }
+            }
         }
 
         private void SetCells()
@@ -135,6 +143,134 @@ namespace FourCharacterPhrase.Shared
         private void ChangeCellsStatusSelectingToCompleted()
         {
             Cells.Where(m => m.Status == CellStatus.Selecting).ToList().ForEach(m => m.Status = CellStatus.Completed);
+        }
+
+        private void SetAllWords()
+        {
+            AllWords = new List<string>();
+
+            AllWords.Add("悪戦苦闘");
+            AllWords.Add("暗中模索");
+            AllWords.Add("唯唯諾諾");
+            AllWords.Add("意気消沈");
+            AllWords.Add("異口同音");
+            AllWords.Add("意志薄弱");
+            AllWords.Add("一網打尽");
+            AllWords.Add("一攫千金");
+            AllWords.Add("一喜一憂");
+            AllWords.Add("一騎当千");
+            AllWords.Add("一石二鳥");
+            AllWords.Add("威風堂堂");
+            AllWords.Add("因果応報");
+            AllWords.Add("右往左往");
+            AllWords.Add("有象無象");
+            AllWords.Add("紆余曲折");
+            AllWords.Add("汚名返上");
+            AllWords.Add("温故知新");
+            AllWords.Add("開口一番");
+            AllWords.Add("花鳥風月");
+            AllWords.Add("冠婚葬祭");
+            AllWords.Add("危機一髪");
+            AllWords.Add("起死回生");
+            AllWords.Add("起承転結");
+            AllWords.Add("疑心暗鬼");
+            AllWords.Add("奇想天外");
+            AllWords.Add("喜怒哀楽");
+            AllWords.Add("九死一生");
+            AllWords.Add("狂喜乱舞");
+            AllWords.Add("共存共栄");
+            AllWords.Add("玉石混交");
+            AllWords.Add("空前絶後");
+            AllWords.Add("九分九厘");
+            AllWords.Add("荒唐無稽");
+            AllWords.Add("極悪非道");
+            AllWords.Add("国士無双");
+            AllWords.Add("古今東西");
+            AllWords.Add("五臓六腑");
+            AllWords.Add("五里霧中");
+            AllWords.Add("言語道断");
+            AllWords.Add("三寒四温");
+            AllWords.Add("自画自賛");
+            AllWords.Add("時期尚早");
+            AllWords.Add("自給自足");
+            AllWords.Add("四苦八苦");
+            AllWords.Add("試行錯誤");
+            AllWords.Add("自業自得");
+            AllWords.Add("時代錯誤");
+            AllWords.Add("十中八九");
+            AllWords.Add("自暴自棄");
+            AllWords.Add("四方八方");
+            AllWords.Add("四面楚歌");
+            AllWords.Add("弱肉強食");
+            AllWords.Add("縦横無尽");
+            AllWords.Add("終始一貫");
+            AllWords.Add("自由自在");
+            AllWords.Add("十人十色");
+            AllWords.Add("取捨選択");
+            AllWords.Add("酒池肉林");
+            AllWords.Add("春夏秋冬");
+            AllWords.Add("順風満帆");
+            AllWords.Add("支離滅裂");
+            AllWords.Add("人海戦術");
+            AllWords.Add("心機一転");
+            AllWords.Add("新陳代謝");
+            AllWords.Add("森羅万象");
+            AllWords.Add("生殺与奪");
+            AllWords.Add("清廉潔白");
+            AllWords.Add("切磋琢磨");
+            AllWords.Add("絶体絶命");
+            AllWords.Add("千載一遇");
+            AllWords.Add("前代未聞");
+            AllWords.Add("全知全能");
+            AllWords.Add("大義名分");
+            AllWords.Add("大言壮語");
+            AllWords.Add("大胆不敵");
+            AllWords.Add("大同小異");
+            AllWords.Add("他力本願");
+            AllWords.Add("貪官汚吏");
+            AllWords.Add("断章取義");
+            AllWords.Add("単刀直入");
+            AllWords.Add("魑魅魍魎");
+            AllWords.Add("猪突猛進");
+            AllWords.Add("適材適所");
+            AllWords.Add("徹頭徹尾");
+            AllWords.Add("天下一品");
+            AllWords.Add("電光石火");
+            AllWords.Add("天真爛漫");
+            AllWords.Add("東西南北");
+            AllWords.Add("東奔西走");
+            AllWords.Add("難攻不落");
+            AllWords.Add("二者択一");
+            AllWords.Add("二束三文");
+            AllWords.Add("日進月歩");
+            AllWords.Add("年功序列");
+            AllWords.Add("拍手喝采");
+            AllWords.Add("馬耳東風");
+            AllWords.Add("八方美人");
+            AllWords.Add("波瀾万丈");
+            AllWords.Add("罵詈雑言");
+            AllWords.Add("品行方正");
+            AllWords.Add("風林火山");
+            AllWords.Add("不倶戴天");
+            AllWords.Add("粉骨砕身");
+            AllWords.Add("文武両道");
+            AllWords.Add("平穏無事");
+            AllWords.Add("平平凡凡");
+            AllWords.Add("暴飲暴食");
+            AllWords.Add("満身創痍");
+            AllWords.Add("万目睚眥");
+            AllWords.Add("三日天下");
+            AllWords.Add("三日坊主");
+            AllWords.Add("無病息災");
+            AllWords.Add("問答無用");
+            AllWords.Add("悠悠自適");
+            AllWords.Add("油断大敵");
+            AllWords.Add("竜頭蛇尾");
+            AllWords.Add("良妻賢母");
+            AllWords.Add("臨機応変");
+            AllWords.Add("劣勢挽回");
+            AllWords.Add("老若男女");
+
         }
     }
 }
